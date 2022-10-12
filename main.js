1c0a482c3f52b9f04117c75d36568c16a8ae5c0e
Ayush <87942664+iayushsahu@users.noreply.github.com>
2022-10-13 02:22:49 +0530

    Add files via upload
    artist playlist did also working

--- main.js
+++ main.js
@@ -81,6 +81,8 @@ document.getElementById("setting_icon").onmouseout = function () {
   document.getElementById("setting_icon").style.filter = "contrast(0)";
 };
 
+// top mix playlist mouse events
+
 const element = document.querySelector(".top_mix_playlist_play_button");
 element.classList.add(
   "animate__animated",
@@ -99,6 +101,130 @@ document.querySelector(".top_mix_playlist1").onmouseout = function () {
     ".top_mix_playlist_play_button_wrapper1"
   ).style.display = "none";
 };
+document.querySelector(".top_mix_playlist2").onmouseover = function () {
+  document.querySelector(
+    ".top_mix_playlist_play_button_wrapper2"
+  ).style.display = "block";
+};
+
+document.querySelector(".top_mix_playlist2").onmouseout = function () {
+  document.querySelector(
+    ".top_mix_playlist_play_button_wrapper2"
+  ).style.display = "none";
+};
+document.querySelector(".top_mix_playlist2").onmouseover = function () {
+  document.querySelector(
+    ".top_mix_playlist_play_button_wrapper2"
+  ).style.display = "block";
+};
+
+document.querySelector(".top_mix_playlist3").onmouseout = function () {
+  document.querySelector(
+    ".top_mix_playlist_play_button_wrapper3"
+  ).style.display = "none";
+};
+document.querySelector(".top_mix_playlist3").onmouseover = function () {
+  document.querySelector(
+    ".top_mix_playlist_play_button_wrapper3"
+  ).style.display = "block";
+};
+
+document.querySelector(".top_mix_playlist4").onmouseout = function () {
+  document.querySelector(
+    ".top_mix_playlist_play_button_wrapper4"
+  ).style.display = "none";
+};
+document.querySelector(".top_mix_playlist4").onmouseover = function () {
+  document.querySelector(
+    ".top_mix_playlist_play_button_wrapper4"
+  ).style.display = "block";
+};
+
+document.querySelector(".top_mix_playlist5").onmouseout = function () {
+  document.querySelector(
+    ".top_mix_playlist_play_button_wrapper5"
+  ).style.display = "none";
+};
+document.querySelector(".top_mix_playlist5").onmouseover = function () {
+  document.querySelector(
+    ".top_mix_playlist_play_button_wrapper5"
+  ).style.display = "block";
+};
+
+document.querySelector(".top_mix_playlist6").onmouseout = function () {
+  document.querySelector(
+    ".top_mix_playlist_play_button_wrapper6"
+  ).style.display = "none";
+};
+document.querySelector(".top_mix_playlist6").onmouseover = function () {
+  document.querySelector(
+    ".top_mix_playlist_play_button_wrapper6"
+  ).style.display = "block";
+};
+
+document.querySelector(".top_mix_playlist7").onmouseout = function () {
+  document.querySelector(
+    ".top_mix_playlist_play_button_wrapper7"
+  ).style.display = "none";
+};
+document.querySelector(".top_mix_playlist7").onmouseover = function () {
+  document.querySelector(
+    ".top_mix_playlist_play_button_wrapper7"
+  ).style.display = "block";
+};
+
+// artist playlist mouse Event
+
+document.querySelector(".artist_div1").onmouseover = function () {
+  document.querySelector(".artist_div_btn_wrapper1").style.display = "block";
+};
+document.querySelector(".artist_div1").onmouseout = function () {
+  document.querySelector(".artist_div_btn_wrapper1").style.display = "none";
+};
+
+document.querySelector(".artist_div2").onmouseover = function () {
+  document.querySelector(".artist_div_btn_wrapper2").style.display = "block";
+};
+document.querySelector(".artist_div2").onmouseout = function () {
+  document.querySelector(".artist_div_btn_wrapper2").style.display = "none";
+};
+
+document.querySelector(".artist_div3").onmouseover = function () {
+  document.querySelector(".artist_div_btn_wrapper3").style.display = "block";
+};
+document.querySelector(".artist_div3").onmouseout = function () {
+  document.querySelector(".artist_div_btn_wrapper3").style.display = "none";
+};
+
+document.querySelector(".artist_div4").onmouseover = function () {
+  document.querySelector(".artist_div_btn_wrapper4").style.display = "block";
+};
+document.querySelector(".artist_div4").onmouseout = function () {
+  document.querySelector(".artist_div_btn_wrapper4").style.display = "none";
+};
+
+document.querySelector(".artist_div5").onmouseover = function () {
+  document.querySelector(".artist_div_btn_wrapper5").style.display = "block";
+};
+document.querySelector(".artist_div5").onmouseout = function () {
+  document.querySelector(".artist_div_btn_wrapper5").style.display = "none";
+};
+
+document.querySelector(".artist_div6").onmouseover = function () {
+  document.querySelector(".artist_div_btn_wrapper6").style.display = "block";
+};
+document.querySelector(".artist_div6").onmouseout = function () {
+  document.querySelector(".artist_div_btn_wrapper6").style.display = "none";
+};
+
+document.querySelector(".artist_div7").onmouseover = function () {
+  document.querySelector(".artist_div_btn_wrapper7").style.display = "block";
+};
+document.querySelector(".artist_div7").onmouseout = function () {
+  document.querySelector(".artist_div_btn_wrapper7").style.display = "none";
+};
+
+// player javascript for music play and pause
 
 document.getElementById("song_like").onclick = () => {
   document.getElementById(
@@ -106,36 +232,32 @@ document.getElementById("song_like").onclick = () => {
   ).innerHTML = `<ion-icon name="heart"></ion-icon>`;
 };
 
-
 const playerButton = document.getElementById("play_pause"),
   audio = document.querySelector("audio"),
   playIcon = `<ion-icon name="play" style="color: var(--icons-color);"></ion-icon>`,
   pauseIcon = `<ion-icon style="color: var(--icons-color);" name="pause"></ion-icon>`;
 
-  function toggleAudio () {
-    if (audio.paused) {
-      audio.play();
-      playerButton.innerHTML = pauseIcon;
-    } else {
-      audio.pause();
-      playerButton.innerHTML = playIcon;
-    }
-  }
-  playerButton.addEventListener('click', toggleAudio);
-
-  function audioEnded () {
+function toggleAudio() {
+  if (audio.paused) {
+    audio.play();
+    playerButton.innerHTML = pauseIcon;
+  } else {
+    audio.pause();
     playerButton.innerHTML = playIcon;
   }
-  
-  audio.onended = audioEnded;
+}
+playerButton.addEventListener("click", toggleAudio);
+
+function audioEnded() {
+  playerButton.innerHTML = playIcon;
+}
+
+audio.onended = audioEnded;
 
 const timeline = document.querySelector(".timeline");
-function changeTimelinePosition () {
-  const percentagePosition = (100*audio.currentTime) / audio.duration;
+function changeTimelinePosition() {
+  const percentagePosition = (100 * audio.currentTime) / audio.duration;
   timeline.style.backgroundSize = `${percentagePosition}% 100%`;
   timeline.value = percentagePosition;
 }
 audio.ontimeupdate = changeTimelinePosition;
-
-
-
